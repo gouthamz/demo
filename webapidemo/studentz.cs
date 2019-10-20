de37ff74dd560c827c5c4e506516df0f86072c91
gouthamz <50258676+gouthamz@users.noreply.github.com>
2019-10-20 14:17:04 +0530

    Update studentz.cs

--- webapidemo/studentz.cs
+++ webapidemo/studentz.cs
@@ -4,7 +4,7 @@
 //
 //     Manual changes to this file may cause unexpected behavior in your application.
 //     Manual changes to this file will be overwritten if the code is regenerated.
-// </auto-generated>
+// </it is auto-generated>
 //------------------------------------------------------------------------------
 
 namespace webapidemo
